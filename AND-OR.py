7170ce7c40b6dd1445314da71004ffb2e5b1bbf8
sethdahanayake-tech <seth.dahanayake@gmail.com>
2026-06-14 11:09:28 +1200

    Add files via upload

---
+++ AND-OR.py
@@ -0,0 +1,25 @@
+#Write a program to check whether the given values have boolean values or not.
+a = 10
+b = 12 
+c = 0
+
+if a and b and c:
+    print("All the numbers have boolean values as True")
+else:
+    print("Atleast one number has boolean value as False")
+
+a = 10
+b = -10
+c = 0
+
+if a > 0 or c > 0:
+    print("Either of the numbers is greater than 0")
+else:
+    print("No number is greater than 0")
+
+if b > 0 or c > 0:
+    print("Either of the numbers is greater than 0")
+else:
+    print("No number is greater than 0")
+
+    
